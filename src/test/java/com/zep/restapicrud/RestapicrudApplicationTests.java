bf7a09c1664a49af404b8bf5d283d2ded8c8d8c0
Felix Ojiambo <felixojiamboe@gmail.com>
2024-02-10 00:26:54 -0800

    added folders

---
+++ src/test/java/com/zep/restapicrud/RestapicrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zep.restapicrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapicrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
