a725f9671c0dcf30b825a42d94e7dc5fd05cba34
Felix Ojiambo <felixojiamboe@gmail.com>
2024-02-10 01:09:27 -0800

    created post and get methods

--- src/main/java/com/zep/restapicrud/controller/VendorApiService.java
+++ src/main/java/com/zep/restapicrud/controller/VendorApiService.java
@@ -1,16 +1,20 @@
 package com.zep.restapicrud.controller;
 
 import com.zep.restapicrud.model.Vendor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/vendor")
 public class VendorApiService
 {
+    Vendor cloudVendor;
     @GetMapping("{vendorId}")
     public Vendor getVendorDetails(String vendorId){
-    return new Vendor("A1","Oracle 1","Address 1","xxxxxxx");
+    return  cloudVendor;//new Vendor("A1","Oracle 1","Address 1","xxxxxxx");
+    }
+   @PostMapping
+    public  String createVendorDetails(@RequestBody Vendor cloudVendor){
+        this.cloudVendor=cloudVendor;
+        return  "Cloud Vendor Created Succesfully";
     }
 }
